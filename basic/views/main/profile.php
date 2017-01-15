e479028b2714065e6b5f64ad24af5d3d7e1bec4d
Volodymyr <bondarenko_vl@ukr.net>
2017-01-15 22:57:36 +0200

    Single commit

---
+++ basic/views/main/profile.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Profile */
+/* @var $form ActiveForm */
+?>
+<div class="main-profile">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+    <?php
+    if($model->user)
+        echo $form->field($model->user, 'username');
+    ?>
+    <?= $form->field($model, 'first_name') ?>
+        <?= $form->field($model, 'second_name') ?>
+        <?= $form->field($model, 'middle_name') ?>
+        <?= $form->field($model, 'avatar')->fileInput() ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Редактировать', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div>
+
+
+
+<!-- main-profile -->
